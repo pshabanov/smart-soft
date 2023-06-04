7214f8387f49cd9fab2bc393e2e4b15f7562eb68
Pavuk <fantasydarth031@gmail.com>
2023-06-04 17:35:44 +0600

    home page

---
+++ web/resources/views/home.blade.php
@@ -0,0 +1,12 @@
+@extends('layout.app')
+@section('title', 'Главная страница')
+@section('content')
+
+    @include('blocks.header')
+    <div class="grid grid-cols-1 md:grid-cols-3 gap-10 mt-10 mb-20">
+        123
+    </div>
+
+@endsection
+
+
---
+++ web/resources/views/layout/app.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite('resources/css/app.css')
+    <title>@yield('title')</title>
+</head>
+<body class="h-screen">
+
+@yield('content')
+
+@vite('resources/js/app.js')
+
+</body>
+</html>
