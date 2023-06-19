e92f3508d911700b1f64deadd2bb188189477fb9
Pavuk <fantasydarth031@gmail.com>
2023-06-20 04:13:07 +0600

    services list and detail *db

---
+++ web/app/Http/Controllers/ServicesController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Service;
+use Illuminate\Http\Request;
+
+class ServicesController extends Controller
+{
+    public function index(){
+        $services = Service::query()->orderBy("created_at", 'desc')->get();
+        return view('pages.services.services_list',[
+            'services' => $services
+        ]);
+    }
+
+    public function detail($id){
+        $services = Service::query()->findOrFail($id);
+        return view('pages.services.detail_services',[
+            'services'=>$services
+        ]);
+    }
+}
