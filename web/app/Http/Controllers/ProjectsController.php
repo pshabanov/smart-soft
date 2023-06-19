a68ed3a9a7c9a55e4e4c2337faeffea4fed38897
Pavuk <fantasydarth031@gmail.com>
2023-06-20 02:59:18 +0600

    projects list and detail

--- web/app/Http/Controllers/ProjectsController.php
+++ web/app/Http/Controllers/ProjectsController.php
@@ -2,15 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Project;
 use Illuminate\Http\Request;
 
 class ProjectsController extends Controller
 {
     public function index(){
-        return view('pages.projects.projects');
+        $projects = Project::query()->orderBy("created_at", 'desc')->simplePaginate(5);
+        return view('pages.projects.projects_list',[
+            'projects' => $projects
+        ]);
     }
 
-    public function detail(){
-        return view('pages.projects.detail_projects');
+    public function detail($id){
+        $projects = Project::query()->findOrFail($id);
+        return view('pages.projects.detail_projects',[
+            'projects'=>$projects
+        ]);
     }
 }
---
+++ web/resources/views/pages/projects/projects_list.blade.php
@@ -0,0 +1,21 @@
+@extends('layout.app')
+@section('title', 'Решения')
+@section('content')
+
+    @include('blocks.header')
+
+    <main class="py-6">
+        <div class="wrapper">
+            <section class="case">
+                <h1>Наши проекты</h1>
+                <div class="news__list">
+                    @each('pages.projects.partials.item', $projects, 'projects')
+                </div>
+                {{ $projects->links() }}
+            </section>
+        </div>
+    </main>
+
+    @include('blocks.footer')
+
+@endsection
